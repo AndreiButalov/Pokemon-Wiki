7c3ec6f14d7828fe559a4d04851d2e41b749bc26
AndrejYaotsin <andrej.butalov@gmx.de>
2024-04-24 13:55:19 +0200

    start_site

--- script.js
+++ script.js
@@ -1,26 +1,48 @@
 async function init() {
-    let url = `https://pokeapi.co/api/v2/pokemon/ditto`;
+    // let url2 = `https://pokeapi.co/api/v2/pokemon/bulbasaur`;
+    let url = `https://pokeapi.co/api/v2/pokemon`;
     let response = await fetch(url);
     let responseAsJson = await response.json();
 
     let post = document.getElementById('post');
 
-    post.innerHTML = /*html*/`
-        <div class="pokemon_headline">
-            <h1 class="pokemon_name">${responseAsJson['name']}</h1>
-            <img src="${responseAsJson['sprites']['front_shiny']}" alt="">
-        </div>
-        <div class="pokemon_info"> 
-            <h1>Poxedex Data</h1>
-            <h2>Category</h2>
-            <span>Height</span>
-            <span>Weight</span>
-            <span>Capture Rate</span>
-            <span>Category</span>
-        </div>
-    `;
+    let results = responseAsJson['results'];
 
-    console.log(responseAsJson);
+    // console.log(results[0]['name']);
+
+    for (let i = 0; i < results.length; i++)  {
+
+        let pokemon_image = `https://pokeapi.co/api/v2/pokemon/${results[i]['name']}`
+        let pokemon_response = await fetch(pokemon_image);
+        let json_pokemon = await pokemon_response.json();
+
+        // console.log(pokemon_response)
+        post.innerHTML += /*html*/ `            
+            <div class="start_app">
+                <div class="pokemon_name">
+                    ${results[i]['name']}
+                </div>
+                <img src="${json_pokemon['sprites']['front_shiny']}" alt="">
+            </div>
+        `;  
+    }
+    
+
+
+
+
+    // post.innerHTML = /*html*/`
+    //     <div class="pokemon_headline">
+    //         <h1 class="pokemon_name">${responseAsJson['name']}</h1>
+    //         <img src="${responseAsJson['sprites']['front_shiny']}" alt="">
+    //     </div>
+    //     <div class="pokemon_info"> 
+    //         <h1>Poxedex Data</h1>
+    //         <h2>Category</h2>            
+    //     </div>
+    // `;
+
+    // console.log(responseAsJson);
 
 
     // sprites/front_shiny
